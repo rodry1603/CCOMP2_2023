fce329759a5f67a46a2e79fcfdd594e5af206a1a
rodry1603 <112088811+rodry1603@users.noreply.github.com>
2023-09-29 23:12:56 -0500

    Add files via upload

---
+++ multiplicacion_matrices.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void multi(int(*p)[3][3]){
+	int (*res)[3]=*(p+2);
+	int (*q)[3]=*(p+1);
+	int (*x)[3]=*p;
+	while(x<*(p+1)){
+	    for(int *z=*x;z<*(x+1);z++){
+	      for(int*y=*q,*t=*res ;y<*(q+1)&&t<*(res+1);y++,t++){
+	        *t+=(*z)*(*y);
+	      }
+	      q++;
+	    }
+	    q=*(p+1);
+	    x++;
+	    res++;
+	}
+	/*imprimir*/
+    res=*(p+2);
+	while(res<*(p+3)){
+    	for(int*g=*res;g<*(res+1);g++){
+        	cout<< *g << " ";
+    	}
+		cout<<endl;
+		res++;
+	}
+	
+}
+	
+int main(){
+	int A[3][3][3]={{{2,0,1},{3,0,0},{5,1,1}},{{1,0,1},{1,2,1},{1,1,0}},{{0,0,0},{0,0,0},{0,0,0}}};
+	multi(A);
+}
